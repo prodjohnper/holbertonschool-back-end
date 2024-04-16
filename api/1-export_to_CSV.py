87421ec1d490dbdd3946408f0301f38b7fecfb99
prodjohnper <JonathanPerez9743@gmail.com>
2024-04-16 09:50:55 -0400

    AUpdate - Refactored code

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,54 +1,75 @@
 #!/usr/bin/python3
 '''
-    Python script that, using this REST API, for a given employee ID,
-    returns information about his/her TODO list progress and export data
-    in the CSV format.
+    Script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress and
+    export it to CSV.
 '''
+
 import csv
 import json
 import requests
 from sys import argv
 
 
-def get_data():
+def get_employee(id=None):
     '''
-        Function that returns information about his/her TODO
-        list progress and export data in the CSV format.
+        Function that, using this REST API, for a given employee ID,
+        returns information about his/her TODO list progress and export
+        it to CSV.
     '''
-    # Get the employee ID
-    employee_id = int(argv[1])
+    # Check if the argument is a number
+    if len(argv) > 1:
+        try:
+            id = int(argv[1])
+        except ValueError:
+            pass
+            return
 
-    # Get the employee name
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    response = requests.get(url)
-    employee = response.json()
+    # Check if the argument is a number
+    if isinstance(id, int):
+        user = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
+        to_dos = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
+        )
 
-    # Get the employee's tasks
-    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        employee_id)
-    response = requests.get(url)
-    todos = response.json()
+        # Check if the request was successful
+        if to_dos.status_code == 200 and user.status_code == 200:
+            user = json.loads(user.text)
+            to_dos = json.loads(to_dos.text)
+            total_tasks = len(to_dos)
+            tasks_completed = 0
+            titles_completed = []
+            csv_rows = []
+            user_id = id
 
-    # Print the employee's tasks
-    done_tasks = []
-    for task in todos:
-        if task.get('completed'):
-            done_tasks.append(task.get('title'))
-    print('Employee {} is done with tasks({}/{}):'.format(
-        employee.get('name'), len(done_tasks), len(todos)))
+            # Append data to csv_rows
+            for to_do in to_dos:
+                csv_rows.append(
+                    [user_id, user['username'],
+                     to_do['completed'],
+                     to_do['title']]
+                )
 
-    # Print the tasks
-    for task in done_tasks:
-        print('\t {}'.format(task))
+                # Count and append titles of completed tasks
+                if to_do['completed'] is True:
+                    tasks_completed += 1
+                    titles_completed.append(to_do['title'])
 
-    # Export data in the CSV format
-    with open('{}.csv'.format(employee_id), mode='w') as employee_file:
-        employee_writer = csv.writer(employee_file, delimiter=',',
-                                     quotechar='"', quoting=csv.QUOTE_ALL)
-        for task in todos:
-            employee_writer.writerow([employee_id, employee.get('username'),
-                                     task.get('completed'), task.get('title')])
+            tasks_completed = len(titles_completed)
+
+            # Print the data
+            print(f"Employee {user['name']} is done \
+                  with tasks({tasks_completed}/{total_tasks})")
+
+            # Print the titles of completed tasks
+            for title in titles_completed:
+                print('\t {}'.format(title))
+
+            # Export data to CSV
+            with open(f"{user_id}.csv", 'w', newline='') as csv_file:
+                writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+                writer.writerows(csv_rows)
 
 
 if __name__ == '__main__':
-    get_data()
+    get_employee()
